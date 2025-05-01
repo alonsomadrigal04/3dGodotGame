86977ef42b38fa1ef24ec17bc8a2dc37043b9e38
Alonso Madrigal <alonso.madrigal.h@gmail.com>
2025-05-01 14:24:25 +0200

    Dead scene v2

--- Scenes/AnimationDeadScreen.cs
+++ Scenes/AnimationDeadScreen.cs
@@ -4,12 +4,17 @@ using System;
 public partial class AnimationDeadScreen : Control
 {
     [Export] private RichTextLabel deadText;
+    [Export] private float waitTimeBeforeSlide = 2.0f;
+    [Export] private float slideDistance = 150f;
+    [Export] private float slideDuration = 0.5f;
+    [Export] private Button startAgain;
+    [Export] private Button exit;
 
     private string[] deathMessages = new string[]
     {
         "You have literally \n[color=#c21807][shake rate=20.0 level=20 connected=1]DIED[/shake][/color]",
         "Well...\n[color=red][wave freq=10 amp=20]That was quick[/wave][/color]",
-        "Game over, man!\n[color=darkred][shake]You're toast[/shake][/color]",
+        "Game over, man!\n[color=darkred][shake]You’re toast[/shake][/color]",
         "Hope you had fun!\n[color=crimson][rainbow freq=4 sat=0.5 val=0.8]NOT.[/rainbow][/color]",
         "RIP little fly\n[color=orangered][shake rate=15 level=15]squashed[/shake][/color]"
     };
@@ -17,19 +22,49 @@ public partial class AnimationDeadScreen : Control
     public override void _Ready()
     {
         ShowDeathText();
+        AudioManager.Instance.PlayMusic("DeadMusic");
     }
 
-    public void ShowDeathText()
+    public async void ShowDeathText()
     {
-        // Elegir un texto aleatorio
         string selectedMessage = deathMessages[GD.Randi() % deathMessages.Length];
         deadText.Text = selectedMessage;
-        deadText.VisibleCharacters = -1; // Asegura que se muestre todo el texto
-        deadText.Scale = new Vector2(3f, 3f); // Empieza grande
+        deadText.VisibleCharacters = -1;
+        deadText.Scale = new Vector2(3f, 3f);
 
-        // Crear tween para "chocar" con la pantalla
         Tween tween = GetTree().CreateTween();
         tween.TweenProperty(deadText, "scale", new Vector2(1f, 1f), 0.4f)
+            .SetTrans(Tween.TransitionType.Elastic)
+            .SetEase(Tween.EaseType.Out);
+
+        await ToSignal(GetTree().CreateTimer(waitTimeBeforeSlide), "timeout");
+
+        // Slide text
+        Vector2 finalPosition = deadText.Position - new Vector2(0, slideDistance);
+        Tween moveTween = GetTree().CreateTween();
+        moveTween.TweenProperty(deadText, "position", finalPosition, slideDuration)
+            .SetTrans(Tween.TransitionType.Cubic)
+            .SetEase(Tween.EaseType.Out);
+
+        // Llama a los botones justo al iniciar el slide
+        SummonButtons();
+    }
+
+    private void SummonButtons()
+    {
+        startAgain.Visible = true;
+        exit.Visible = true;
+
+        startAgain.Scale = new Vector2(0, 0);
+        exit.Scale = new Vector2(0, 0);
+
+        Tween tween = GetTree().CreateTween();
+
+        tween.Parallel().TweenProperty(startAgain, "scale", new Vector2(1, 1), 0.5f)
+            .SetTrans(Tween.TransitionType.Elastic)
+            .SetEase(Tween.EaseType.Out);
+
+        tween.Parallel().TweenProperty(exit, "scale", new Vector2(1, 1), 0.5f)
             .SetTrans(Tween.TransitionType.Elastic)
             .SetEase(Tween.EaseType.Out);
     }
